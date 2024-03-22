3ea8ad690f700c593a00545aebc3d2b13615e529
WuMaxtw <WuMax13@gmail.com>
2024-03-22 18:34:36 +0800

    hw0104

--- hw01/hw0106.c
+++ hw01/hw0106.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
-typedef unsigned _BitInt(256) u256;
-typedef /*signed*/ _BitInt(256) i256;
-
-// works with clang 16
-// but not GCC yet (as of April 2023)
-
-int lt0(i256 a){
-    return a < 0;  // just the sign bit in the top limb
-}
-
-// -march=broadwell allows mulx which clang uses, and adox/adcx which it doesn't
-u256 mul256(u256 a, u256 b){
-    return a*b;
-}
 int main(){
-
+	_BitInt(4) x;
 
     return 0;
 }
